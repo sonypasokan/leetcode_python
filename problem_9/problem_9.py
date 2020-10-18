9bbc81fa30032375b37cd758ca243702159a3514
Sony A P <sonyap@Sonys-MacBook-Air.local>
2020-10-18 19:34:26 +0530

    Solved problem 9 of leet code (find the number is a palindrome or not)

---
+++ problem_9/problem_9.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        number = x
+        reverse_number = 0
+        while number > 0:
+            remainder = number % 10
+            reverse_number = reverse_number * 10 + remainder
+            number = number // 10
+            print("actual number=", x, "number in this iteration=", number, "reverse:", reverse_number)
+        return x == reverse_number
+        
+if __name__ == "__main__":
+    input_list = [
+        -121,
+        -101,
+        121
+    ]
+    for input_item in input_list:
+        print("Input=", input_item)
+        output = Solution().isPalindrome(input_item)
+        print("Output=", output, "\n")
