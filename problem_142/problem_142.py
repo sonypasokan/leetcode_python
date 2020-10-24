75b0aa508982fd4d939ecbf9687f8a985098bef2
Sony A P <sonyap@Sonys-Air.domain.name>
2020-10-24 22:19:59 +0530

    Solved problem 142 using Hash map

---
+++ problem_142/problem_142.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        node_map = dict()
+        node = head
+
+        while node != None:
+            if node in node_map:
+                return node
+            node_map[node] = None
+            node = node.next
+        
+        return None
